e5cd6385e3ab865e9baa4570e42644c913ae10a0
https://github.com/Sudhishkumartiwari <Sudhishktiwari2004@gmail.com>
2026-01-12 16:05:01 +0530

    Day 5: GreetingSolution using Class Library and Console App

---
+++ Day5/GreetingLibrary/GreetingHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Hello, Guest!";
+            }
+            else
+            {
+                return "Hello, " + name + "!";
+            }
+        }
+    }
+}
