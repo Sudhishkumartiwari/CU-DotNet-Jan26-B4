0db69beff29072cc0c669b0d0074b8fbc22d1cf9
https://github.com/Sudhishkumartiwari <Sudhishktiwari2004@gmail.com>
2026-01-12 15:44:32 +0530

    Day 5: Shared Greeting Library using Class Library and Console App

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using GreetingLibrary;
+
+namespace GreetingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            string greeting = GreetingHelper.GetGreeting(name);
+
+            Console.WriteLine(greeting);
+            Console.ReadKey();
+        }
+    }
+}
