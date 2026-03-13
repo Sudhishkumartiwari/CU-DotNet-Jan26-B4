1ec50870cc0bffefc48a8713fce96f55a4098be9
Sudhish tiwari <sudhishktiwari2004@gmail.com>
2026-03-13 09:16:54 +0000

    Dashboard employee

---
+++ Assignments/Day55/Controllers/CompanyController.cs
@@ -0,0 +1,29 @@
+﻿using DashBoard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DashBoard.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            // Simulating database data
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee { EmployeeId = 1, Name = "Rahul Sharma", Position = "Software Engineer", Salary = 60000 },
+                new Employee { EmployeeId = 2, Name = "Priya Singh", Position = "UI Designer", Salary = 55000 },
+                new Employee { EmployeeId = 3, Name = "Aman Verma", Position = "QA Analyst", Salary = 50000 },
+                new Employee { EmployeeId = 4, Name = "Riya Kapoor", Position = "Project Manager", Salary = 75000 }
+            };
+
+            // ViewBag → Announcement
+            ViewBag.Announcement = "🎉 Company Meeting Today at 4 PM";
+
+            // ViewData → Static configuration
+            ViewData["DepartmentName"] = "Software Development";
+            ViewData["ServerStatus"] = true;
+
+            return View(employees);
+        }
+    }
+}
---
+++ Assignments/Day55/Data/DashBoardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DashBoard.Models;
+
+namespace DashBoard.Data
+{
+    public class DashBoardContext : DbContext
+    {
+        public DashBoardContext (DbContextOptions<DashBoardContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DashBoard.Models.Employee> Employee { get; set; } = default!;
+    }
+}
